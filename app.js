e88d35ad7da2af6dc51adb71409251073590f20d
Tsila <randev117@randev.ovh>
2024-08-26 14:16:25 +0300

    static and dynamic route

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/movies', (req, res) => {
+    res.send('Bientôt des films ici');
+})
+
+app.get('/movies/add', (req, res) => {
+    res.send(`Bientôt un formulaire ici`);
+})
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Film numéro ${id}`);
+})
+
 app.get('/', function (req, res) {
     res.send('Hello <b>world</b>');
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`listening in port ${PORT}`);
 })
