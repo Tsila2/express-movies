13b399499e423e3ac9cd6a1b2bd6375115a47331
Tsila <randev117@randev.ovh>
2024-08-26 13:58:39 +0300

    nodemon

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
+
 app.get('/', function (req, res) {
     res.send('Hello <b>world</b>');
 })
 
-app.listen(3000, function(){
-    console.log('listening in port 3000');
+app.listen(PORT, function(){
+    console.log(`listening in port ${PORT}`);
 })
