5bd53722944a68190d0cccebf9177f9e8fa2850b
Tsila <randev117@randev.ovh>
2024-08-26 14:57:38 +0300

    passing data

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/movies', (req, res) => {
-    res.send('Bientôt des films ici');
+    // res.send('Bientôt des films ici');
+    res.render('movies')
 })
 
 app.get('/movies/add', (req, res) => {
@@ -17,12 +18,14 @@ app.get('/movies/add', (req, res) => {
 
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    res.send(`Film numéro ${id}`);
+    // res.send(`Film numéro ${id}`);
+    const title = "Terminator";
+    res.render('movies-details', { moviesId: id, title: title })
 })
 
 app.get('/', function (req, res) {
     // res.send('Hello <b>world</b>');
-    res.render('index'); 
+    res.render('index');
 })
 
 app.listen(PORT, function () {
